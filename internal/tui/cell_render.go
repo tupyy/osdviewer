9c9a4a1c8a53ca146d7f3bfa0bd44a283e0e083b
Cosmin Tupangiu <cosmin@redhat.com>
2023-10-21 13:11:28 +0200

    add func for padding

--- internal/tui/cell_render.go
+++ internal/tui/cell_render.go
@@ -2,10 +2,17 @@ package tui
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"github.com/tupyy/osdviewer/internal/entity"
+)
+
+const (
+	leftPadding int = iota
+	rightPadding
+	middlePadding
 )
 
 func RenderCell(cluster entity.Cluster, col int) *tview.TableCell {
@@ -24,7 +31,7 @@ func RenderCell(cluster entity.Cluster, col int) *tview.TableCell {
 	case 0:
 		id := cluster.ID()
 		if cluster.Kind() == entity.ManagementClusterType {
-			id = fmt.Sprintf("     %s", id)
+			id = pad(id, 4, leftPadding)
 		}
 		cell = tview.NewTableCell(id).SetExpansion(2)
 	case 1:
@@ -48,4 +55,20 @@ func RenderCell(cluster entity.Cluster, col int) *tview.TableCell {
 	}
 
 	return cell
+}
+
+func pad(text string, count int, side int) string {
+	if count <= 0 {
+		return text
+	}
+	switch side {
+	case leftPadding:
+		return fmt.Sprintf("%s%s", strings.Repeat(" ", count), text)
+	case rightPadding:
+		return fmt.Sprintf("%s%s", text, strings.Repeat(" ", count))
+	case middlePadding:
+		return fmt.Sprintf("%s%s%s", strings.Repeat(" ", count), text, strings.Repeat(" ", count))
+	default:
+		return text
+	}
 }
